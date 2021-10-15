e340888e4d84c464a6ce64eb42eadd7af0bef673
Lukas Zöllner <lks.zoellner@gmail.com>
2021-10-15 23:22:41 +0200

    Calendar card and user card are roughly finished, some bugfixes left to do

--- Module 1/Project 2/js/main.js
+++ Module 1/Project 2/js/main.js
@@ -1,46 +1,20 @@
-// Register Bootstrap Tooltips
-const githubCalendarDays = document.querySelectorAll(".github-calendar-day");
-for (const githubCalendarDay of githubCalendarDays) {
-  new bootstrap.Tooltip(githubCalendarDay);
-}
-
 import { GITHUB_API_KEY } from "../dev/GITHUB_API_KEY.js";
 
-// Github calendar hard coded
-// const endpoint = "https://api.github.com/graphql";
-
-// const headers = new Headers();
-// headers.append("Authorization", `Bearer ${GITHUB_API_KEY}`);
-// headers.append("Content-Type", "application/graphql");
-
-// const query = JSON.stringify({
-//   query:
-//     "query($username: String!) {\n  user(login: $username) {\n    name\n    contributionsCollection {\n      contributionCalendar {\n        colors\n        totalContributions\n        weeks {\n          contributionDays {\n            color\n            contributionCount\n            date\n            weekday\n          }\n          firstDay\n        }\n      }\n    }\n  }\n}",
-//   variables: { username: "irrelevation" },
-// });
-
-// const requestOptions = {
-//   method: "POST",
-//   headers: headers,
-//   body: query,
-//   redirect: "follow",
-// };
-
-// fetch(endpoint, requestOptions)
-//   .then((response) => response.json())
-
-// Github Data
-const fetchGithubData = (username, authToken) => {
+const fetchGithubData = ({ username, authToken, startDate, endDate }) => {
   const endpoint = "https://api.github.com/graphql";
   const headers = new Headers();
   headers.append("Authorization", `Bearer ${authToken}`);
   headers.append("Content-Type", "application/graphql");
 
   const query = `
-    query($username: String!) {
+    query ($username: String!, $startDate: DateTime, $endDate: DateTime) {
       user(login: $username) {
         name
-        contributionsCollection {
+        avatarUrl
+        email
+        twitterUsername
+        url
+        contributionsCollection(from: $startDate, to: $endDate) {
           contributionCalendar {
             colors
             totalContributions
@@ -54,11 +28,20 @@ const fetchGithubData = (username, authToken) => {
               firstDay
             }
           }
+          totalCommitContributions
+          totalIssueContributions
+          totalPullRequestContributions
+          totalPullRequestReviewContributions
+          totalRepositoriesWithContributedCommits
+          totalRepositoriesWithContributedIssues
+          totalRepositoriesWithContributedPullRequestReviews
+          totalRepositoriesWithContributedPullRequests
+          totalRepositoryContributions
         }
       }
     }
   `;
-  const variables = { username };
+  const variables = { username, startDate, endDate };
 
   const requestOptions = {
     method: "POST",
@@ -70,6 +53,38 @@ const fetchGithubData = (username, authToken) => {
   return fetch(endpoint, requestOptions).then(res => res.json());
 };
 
-fetchGithubData("irrelevation", GITHUB_API_KEY)
-  .then((result) => console.log(result))
+const createGithubCalendar = (githubData) => {
+  const contributions = document.querySelector("#contributions");
+  contributions.appendChild(document.createTextNode(githubData.user.contributionsCollection.contributionCalendar.totalContributions));
+  
+  const githubCalendar = document.querySelector(".github-calendar");
+  for (const week of githubData.user.contributionsCollection.contributionCalendar.weeks){
+    for (const weekday of week.contributionDays) {
+      const day = document.createElement("div");
+      day.class = "github-calendar-day";
+      day.dataset.bsToggle = "tooltip";
+      day.dataset.bsPlacement = "top";
+      day.title = `${weekday.contributionCount} contribution${weekday.contributionCount === 1 ? "" : "s"} on ${new Date(weekday.date).toDateString()}`;
+      day.style.backgroundColor = weekday.color;
+      githubCalendar.appendChild(day);
+      const tooltip = new bootstrap.Tooltip(day);
+    }
+  }
+}
+
+const createUserCard = ({avatarUrl, name, email}) => {
+  document.querySelector("#avatar").src = avatarUrl;
+  document.querySelector("#name").innerText = name;
+  document.querySelector("#email").innerText= email;
+}
+
+fetchGithubData({
+  username: "focuscookie",
+  authToken: GITHUB_API_KEY,
+  startDate: "2021-09-01T00:00:00+0000",
+  endDate: "2022-03-01T00:00:00+0000"})
+  .then((result) => {
+    createUserCard(result.data.user);
+    createGithubCalendar(result.data);
+  })
   .catch((error) => console.log("error", error));
